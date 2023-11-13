1cf5c79946b6a3642593406f05ca5ba9c37f9838
ishanpandey <ishan.pandey@infra.market>
2023-11-13 08:17:49 +0530

    add medium sql files

---
+++ sql/medim.sql
@@ -0,0 +1,512 @@
+/*
+Table: Sales
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| sale_date     | date    |
+| fruit         | enum    |
+| sold_num      | int     |
++---------------+---------+
+(sale_date, fruit) is the primary key (combination of columns with unique values) of this table.
+This table contains the sales of "apples" and "oranges" sold each day.
+
+
+Write a solution to report the difference between the number of apples and oranges sold each day.
+
+Return the result table ordered by sale_date.
+
+The result format is in the following example.
+
+Example 1:
+
+Input:
+Sales table:
++------------+------------+-------------+
+| sale_date  | fruit      | sold_num    |
++------------+------------+-------------+
+| 2020-05-01 | apples     | 10          |
+| 2020-05-01 | oranges    | 8           |
+| 2020-05-02 | apples     | 15          |
+| 2020-05-02 | oranges    | 15          |
+| 2020-05-03 | apples     | 20          |
+| 2020-05-03 | oranges    | 0           |
+| 2020-05-04 | apples     | 15          |
+| 2020-05-04 | oranges    | 16          |
++------------+------------+-------------+
+Output:
++------------+--------------+
+| sale_date  | diff         |
++------------+--------------+
+| 2020-05-01 | 2            |
+| 2020-05-02 | 0            |
+| 2020-05-03 | 20           |
+| 2020-05-04 | -1           |
++------------+--------------+
+Explanation:
+Day 2020-05-01, 10 apples and 8 oranges were sold (Difference  10 - 8 = 2).
+Day 2020-05-02, 15 apples and 15 oranges were sold (Difference 15 - 15 = 0).
+Day 2020-05-03, 20 apples and 0 oranges were sold (Difference 20 - 0 = 20).
+Day 2020-05-04, 15 apples and 16 oranges were sold (Difference 15 - 16 = -1).
+*/
+
+-- Solution 1: IF(condition, value_if_true, value_if_false)
+select sale_date,
+       SUM(
+               IF(fruit='apples', sold_num, -sold_num)
+           ) AS diff
+from sales group by sale_date order by sale_date;
+
+-- Solution 2:
+-- CASE  WHEN Col1 < 5 OR Col3 = 4  THEN 1
+--       WHEN Col1 = 3 THEN 2
+--       ...
+--       ELSE 0 END as Qty
+select sale_date,
+       SUM(
+               CASE WHEN fruit='apples' THEN sold_num
+                    ELSE -sold_num
+                   END
+           ) AS diff
+from sales group by sale_date order by sale_date
+
+-- Solution 3:
+with
+apple_sales as (
+    select sale_date, fruit, sold_num as apple_num
+    from Sales
+    where fruit = 'apples'
+),
+orange_sales as (
+    select sale_date, fruit, sold_num as orange_num
+    from Sales
+    where fruit = 'oranges'
+)
+
+select apple_sales.sale_date, apple_num - orange_num as diff from apple_sales, orange_sales where apple_sales.sale_date = orange_sales.sale_date
+order by sale_date
+
+
+/*
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| order_id    | int  |
+| customer_id | int  |
+| order_type  | int  |
++-------------+------+
+order_id is the column with unique values for this table.
+Each row of this table indicates the ID of an order, the ID of the customer who ordered it, and the order type.
+The orders could be of type 0 or type 1.
+
+
+Write a solution to report all the orders based on the following criteria:
+
+If a customer has at least one order of type 0, do not report any order of type 1 from that customer.
+Otherwise, report all the orders of the customer.
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Orders table:
++----------+-------------+------------+
+| order_id | customer_id | order_type |
++----------+-------------+------------+
+| 1        | 1           | 0          |
+| 2        | 1           | 0          |
+| 11       | 2           | 0          |
+| 12       | 2           | 1          |
+| 21       | 3           | 1          |
+| 22       | 3           | 0          |
+| 31       | 4           | 1          |
+| 32       | 4           | 1          |
++----------+-------------+------------+
+Output:
++----------+-------------+------------+
+| order_id | customer_id | order_type |
++----------+-------------+------------+
+| 31       | 4           | 1          |
+| 32       | 4           | 1          |
+| 1        | 1           | 0          |
+| 2        | 1           | 0          |
+| 11       | 2           | 0          |
+| 22       | 3           | 0          |
++----------+-------------+------------+
+Explanation:
+Customer 1 has two orders of type 0. We return both of them.
+Customer 2 has one order of type 0 and one order of type 1. We only return the order of type 0.
+Customer 3 has one order of type 0 and one order of type 1. We only return the order of type 0.
+Customer 4 has two orders of type 1. We return both of them.
+
+*/
+
+-- Solution 1:
+select order_id, customer_id, order_type from Orders where order_type = '0'
+        or (
+            order_type = '1' and customer_id not in (
+                select customer_id from Orders where order_type = '0'
+                )
+            )
+-- other way to write:
+select order_id, customer_id, order_type from orders where customer_id in (select customer_id from orders where order_type='0') and order_type<>'1'
+union
+select order_id, customer_id, order_type from orders where customer_id not in (select customer_id from orders where order_type='0')
+
+/*
+Table: Scores
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| player_name   | varchar |
+| gender        | varchar |
+| day           | date    |
+| score_points  | int     |
++---------------+---------+
+(gender, day) is the primary key (combination of columns with unique values) for this table.
+A competition is held between the female team and the male team.
+Each row of this table indicates that a player_name and with gender has scored score_point in someday.
+Gender is 'F' if the player is in the female team and 'M' if the player is in the male team.
+
+
+Write a solution to find the total score for each gender on each day.
+
+Return the result table ordered by gender and day in ascending order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Scores table:
++-------------+--------+------------+--------------+
+| player_name | gender | day        | score_points |
++-------------+--------+------------+--------------+
+| Aron        | F      | 2020-01-01 | 17           |
+| Alice       | F      | 2020-01-07 | 23           |
+| Bajrang     | M      | 2020-01-07 | 7            |
+| Khali       | M      | 2019-12-25 | 11           |
+| Slaman      | M      | 2019-12-30 | 13           |
+| Joe         | M      | 2019-12-31 | 3            |
+| Jose        | M      | 2019-12-18 | 2            |
+| Priya       | F      | 2019-12-31 | 23           |
+| Priyanka    | F      | 2019-12-30 | 17           |
++-------------+--------+------------+--------------+
+Output:
++--------+------------+-------+
+| gender | day        | total |
++--------+------------+-------+
+| F      | 2019-12-30 | 17    |
+| F      | 2019-12-31 | 40    |
+| F      | 2020-01-01 | 57    |
+| F      | 2020-01-07 | 80    |
+| M      | 2019-12-18 | 2     |
+| M      | 2019-12-25 | 13    |
+| M      | 2019-12-30 | 26    |
+| M      | 2019-12-31 | 29    |
+| M      | 2020-01-07 | 36    |
++--------+------------+-------+
+Explanation:
+For the female team:
+The first day is 2019-12-30, Priyanka scored 17 points and the total score for the team is 17.
+The second day is 2019-12-31, Priya scored 23 points and the total score for the team is 40.
+The third day is 2020-01-01, Aron scored 17 points and the total score for the team is 57.
+The fourth day is 2020-01-07, Alice scored 23 points and the total score for the team is 80.
+
+For the male team:
+The first day is 2019-12-18, Jose scored 2 points and the total score for the team is 2.
+The second day is 2019-12-25, Khali scored 11 points and the total score for the team is 13.
+The third day is 2019-12-30, Slaman scored 13 points and the total score for the team is 26.
+The fourth day is 2019-12-31, Joe scored 3 points and the total score for the team is 29.
+The fifth day is 2020-01-07, Bajrang scored 7 points and the total score for the team is 36.
+*/
+
+-- Solution 1: (sub query) - using self-join to calculate the running aggregated stats.
+select s1.gender, s1.day, sum(s2.score_points) as total from
+Scores as s1,
+Scores as s2
+where s1.gender = s2.gender and s2.day <= s1.day
+group by s1.gender, s1.day
+order by s1.gender, s1.day
+
+-- Solution 2: window function - https://mode.com/sql-tutorial/sql-window-functions/
+select gender, day,
+    sum(score_points) over(partition by gender order by day) as total -- take sum of score_points over (partition by gender, and order by day and take running total, every entry is in result set), order by day
+-- PARTITION BY : groups and orders by gender
+from Scores
+group by gender, day
+
+/*
+Table: OrdersDetails
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| order_id    | int  |
+| product_id  | int  |
+| quantity    | int  |
++-------------+------+
+(order_id, product_id) is the primary key (combination of columns with unique values) for this table.
+A single order is represented as multiple rows, one row for each product in the order.
+Each row of this table contains the quantity ordered of the product product_id in the order order_id.
+
+
+You are running an e-commerce site that is looking for imbalanced orders. An imbalanced order is one whose maximum quantity is strictly greater than the average quantity of every order (including itself).
+
+The average quantity of an order is calculated as (total quantity of all products in the order) / (number of different products in the order). The maximum quantity of an order is the highest quantity of any single product in the order.
+
+Write a solution to find the order_id of all imbalanced orders.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+OrdersDetails table:
++----------+------------+----------+
+| order_id | product_id | quantity |
++----------+------------+----------+
+| 1        | 1          | 12       |
+| 1        | 2          | 10       |
+| 1        | 3          | 15       |
+| 2        | 1          | 8        |
+| 2        | 4          | 4        |
+| 2        | 5          | 6        |
+| 3        | 3          | 5        |
+| 3        | 4          | 18       |
+| 4        | 5          | 2        |
+| 4        | 6          | 8        |
+| 5        | 7          | 9        |
+| 5        | 8          | 9        |
+| 3        | 9          | 20       |
+| 2        | 9          | 4        |
++----------+------------+----------+
+Output:
++----------+
+| order_id |
++----------+
+| 1        |
+| 3        |
++----------+
+Explanation:
+The average quantity of each order is:
+- order_id=1: (12+10+15)/3 = 12.3333333
+- order_id=2: (8+4+6+4)/4 = 5.5
+- order_id=3: (5+18+20)/3 = 14.333333
+- order_id=4: (2+8)/2 = 5
+- order_id=5: (9+9)/2 = 9
+
+The maximum quantity of each order is:
+- order_id=1: max(12, 10, 15) = 15
+- order_id=2: max(8, 4, 6, 4) = 8
+- order_id=3: max(5, 18, 20) = 20
+- order_id=4: max(2, 8) = 8
+- order_id=5: max(9, 9) = 9
+
+Orders 1 and 3 are imbalanced because they have a maximum quantity that exceeds the average quantity of every order.
+*/
+-- Solution 1: sub query
+with ft as (
+    select order_id, max(quantity) as max_qty, avg(quantity) as avg_qty
+    from OrdersDetails group by order_id
+    )
+
+select order_id from ft where max_qty > (select max(avg_qty) from ft);
+
+-- Solution 2: using window function
+-- In max(avg(quantity)) over(), over clause is empty, which treats the entire set of query rows as a single partition. The window function thus produces a global max, but does so for each row.
+with ft as (
+    select order_id, max(quantity) as max_qty, max(avg(quantity)) over() as max_avg_qty
+    from OrdersDetails group by order_id
+)
+
+select order_id from ft where max_qty > max_avg_qty;
+
+/*
+Table: Players
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| player_id      | int     |
+| player_name    | varchar |
++----------------+---------+
+player_id is the primary key (column with unique values) for this table.
+Each row in this table contains the name and the ID of a tennis player.
+
+
+Table: Championships
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| year          | int     |
+| Wimbledon     | int     |
+| Fr_open       | int     |
+| US_open       | int     |
+| Au_open       | int     |
++---------------+---------+
+year is the primary key (column with unique values) for this table.
+Each row of this table contains the IDs of the players who won one each tennis tournament of the grand slam.
+
+
+Write a solution to report the number of grand slam tournaments won by each player. Do not include the players who did not win any tournament.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Players table:
++-----------+-------------+
+| player_id | player_name |
++-----------+-------------+
+| 1         | Nadal       |
+| 2         | Federer     |
+| 3         | Novak       |
++-----------+-------------+
+Championships table:
++------+-----------+---------+---------+---------+
+| year | Wimbledon | Fr_open | US_open | Au_open |
++------+-----------+---------+---------+---------+
+| 2018 | 1         | 1       | 1       | 1       |
+| 2019 | 1         | 1       | 2       | 2       |
+| 2020 | 2         | 1       | 2       | 2       |
++------+-----------+---------+---------+---------+
+Output:
++-----------+-------------+-------------------+
+| player_id | player_name | grand_slams_count |
++-----------+-------------+-------------------+
+| 2         | Federer     | 5                 |
+| 1         | Nadal       | 7                 |
++-----------+-------------+-------------------+
+Explanation:
+Player 1 (Nadal) won 7 titles: Wimbledon (2018, 2019), Fr_open (2018, 2019, 2020), US_open (2018), and Au_open (2018).
+Player 2 (Federer) won 5 titles: Wimbledon (2020), US_open (2019, 2020), and Au_open (2019, 2020).
+Player 3 (Novak) did not win anything, we did not include them in the result table.
+*/
+-- Solution 1: cross join
+select
+    p.player_id,
+    p.player_name,
+    sum(
+                case when p.player_id = c.wimbledon then 1 else 0 end
+                + case when p.player_id = c.fr_open then 1 else 0 end
+                + case when p.player_id = c.us_open then 1 else 0 end
+                + case when p.player_id = c.au_open then 1 else 0 end
+        ) as grand_slams_count
+from Players p cross join Championships c
+group by p.player_id
+having grand_slams_count > 0
+
+
+/* todo: VVIQ
+Table: Employee
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key (column with unique values) for this table.
+departmentId is a foreign key (reference column) of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of a department and its name.
+
+
+A company's executives are interested in seeing who earns the most money in each of the company's departments. A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
+
+Write a solution to find the employees who are high earners in each of the departments.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 85000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
+| 5  | Janet | 69000  | 1            |
+| 6  | Randy | 85000  | 1            |
+| 7  | Will  | 70000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output:
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Joe      | 85000  |
+| IT         | Randy    | 85000  |
+| IT         | Will     | 70000  |
+| Sales      | Henry    | 80000  |
+| Sales      | Sam      | 60000  |
++------------+----------+--------+
+Explanation:
+In the IT department:
+- Max earns the highest unique salary
+- Both Randy and Joe earn the second-highest unique salary
+- Will earns the third-highest unique salary
+
+In the Sales department:
+- Henry earns the highest salary
+- Sam earns the second-highest salary
+- There is no third-highest salary as there are only two employees
+*/
+
+select Department, Employee, Salary from
+    (
+        select
+            e.name as Employee,
+            d.name as Department,
+            e.salary as Salary,
+            dense_rank() over(partition by d.name order by e.salary desc) as salary_rank
+        from Employee e inner join Department d on e.departmentId = d.id
+    ) as dt
+where salary_rank <= 3;
